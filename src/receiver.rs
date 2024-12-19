7c6214787d046826dcdb2270a454bb6fa8313eba
Sergey <sergey.mindblast@mail.ru>
2024-12-20 00:33:45 +0400

    fixes

--- src/receiver.rs
+++ src/receiver.rs
@@ -4,21 +4,21 @@ use nix::mqueue::mq_attr_member_t;
 use nix::mqueue::{mq_close, mq_open, mq_receive, mq_send, MQ_OFlag, MqAttr, MqdT};
 use nix::sys::stat::Mode;
 
-pub struct IpcReceiver<const MESSAGE_SIZE: i64> {
+pub struct IpcReceiver<const MESSAGE_SIZE: usize> {
     descriptor: MqdT,
-    buffer: [u8; MESSAGE_SIZE as usize],
+    buffer: [u8; MESSAGE_SIZE ],
 }
 
-impl<const MESSAGE_SIZE: i64> IpcReceiver<MESSAGE_SIZE> {
+impl<const MESSAGE_SIZE: usize> IpcReceiver<MESSAGE_SIZE> {
     pub fn init(name: &str, capacity: i64) -> Result<Self, Error>
     {
         let flags = MQ_OFlag::O_CREAT | MQ_OFlag::O_RDONLY;
         let mode = Mode::S_IWUSR | Mode::S_IRUSR | Mode::S_IRGRP | Mode::S_IROTH;
-        let attributes = MqAttr::new(0, capacity, MESSAGE_SIZE, 0);
+        let attributes = MqAttr::new(0, capacity, MESSAGE_SIZE as i64, 0);
         let mqd0 = mq_open(name, flags, mode, Some(&attributes))?;
         Ok(Self {
             descriptor: mqd0,
-            buffer: [0; MESSAGE_SIZE as usize],
+            buffer: [0; MESSAGE_SIZE],
         })
     }
 
--- src/sender.rs
+++ src/sender.rs
@@ -4,16 +4,16 @@ use nix::mqueue::mq_attr_member_t;
 use nix::mqueue::{mq_close, mq_open, mq_receive, mq_send, MQ_OFlag, MqAttr, MqdT};
 use nix::sys::stat::Mode;
 
-pub struct IpcSender<const MESSAGE_SIZE: i64> {
+pub struct IpcSender<const MESSAGE_SIZE: usize> {
     descriptor: MqdT,
     sender_id: u8,
 }
 
-impl<const MESSAGE_SIZE: i64> IpcSender<MESSAGE_SIZE> {
+impl<const MESSAGE_SIZE: usize> IpcSender<MESSAGE_SIZE> {
     pub fn init(name: &str, sender_id: u8, capacity: i64) -> Result<Self, Error> {
         let flags = MQ_OFlag::O_WRONLY;
         let mode = Mode::S_IWUSR | Mode::S_IRUSR | Mode::S_IRGRP | Mode::S_IROTH;
-        let attributes = MqAttr::new(0, capacity, MESSAGE_SIZE, 0);
+        let attributes = MqAttr::new(0, capacity, MESSAGE_SIZE as i64, 0);
         let mqd0 = mq_open(name, flags, mode, Some(&attributes))?;
         Ok(Self { descriptor: mqd0, sender_id })
     }
